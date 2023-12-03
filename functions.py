e85b98b232ba93b7c8aae5433d5df1200ddb93cd
dawid <86666763+ddkcz@users.noreply.github.com>
2023-12-03 14:18:14 +0100

    2/x

--- functions.py
+++ functions.py
@@ -1,63 +1,68 @@
-#jednostki metryczne na metr
-def metric(metr, na_metr):
-    match metr:
+###########################################################
+#####                            def_area
+
+#variables
+def variables_definition():
+    #   mm = milimeter
+    #   cm = centimeter
+    #   dm = decyieter
+    #   m = meter
+    #   km = kilometer
+    #
+    #   in = inch
+    #   ft = foot
+    #   yd = yard
+    #   fur = furlong
+    #   mi = #mil
+    pass
+    
+def BaseValueMetric(ValueMetric_m, UnitMetric_m):
+    match UnitMetric_m:
         case "mm":
-            na_metr = na_metr / 1000
+            valueMETER = ValueMetric_m / 1000
         case "cm":
-            na_metr = na_metr / 100
+            valueMETER = ValueMetric_m / 100
         case "dm":
-            na_metr = na_metr / 10
+            valueMETER = ValueMetric_m / 10
         case "m":
-            na_metr = na_metr
+            valueMETER = ValueMetric_m
         case "km":
-            na_metr = na_metr * 1000 
-
-    return na_metr
-
+            valueMETER = ValueMetric_m * 1000 
+    return valueMETER
 
 
-
-#jednostki imperialne na milę
-def imperial(mila, na_mile):
-    match mila:
+def BaseValueImperial(Unit_Imperial_mi, ValueImperial_mi):
+    match Unit_Imperial_mi:
         case "in":
-            na_mile = na_mile * 63360
+            valueMILE = ValueImperial_mi * 63360
         case "ft":
-            na_mile = na_mile * 5.280
+            valueMILE = ValueImperial_mi * 5.280
         case "yd":
-            na_mile = na_mile * 1.760
+            valueMILE = ValueImperial_mi * 1.760
         case "fur":
-            na_mile = na_mile * 5.280
+            valueMILE = ValueImperial_mi * 5.280
         case "mi":
-            na_mile = na_mile
+            valueMILE = ValueImperial_mi
     
-    return na_mile
+    return valueMILE
 
+def UnitMetricConversion(future_Unit_Metric_mic, future_Unit_Metric_mic_value):  
 
-
-
-#metry na jednostki metryczne
-def metr_conversion(future_metric, future_metric_value):  
-
-    match future_metric_value:
+    match future_Unit_Metric_mic_value:
         case "mm":
-            future_metric = future_metric / 1000
+            future_Unit_Metric_mic = future_Unit_Metric_mic / 1000
         case "cm":
-            future_metric = future_metric / 100
+            future_Unit_Metric_mic = future_Unit_Metric_mic / 100
         case "dm":
-            future_metric = future_metric / 10
+            future_Unit_Metric_mic = future_Unit_Metric_mic / 10
         case "m":
-            future_metric = future_metric
+            future_Unit_Metric_mic = future_Unit_Metric_mic
         case "km":
-            future_metricr = future_metric * 1000 
+            future_Unit_Metric_micr = future_Unit_Metric_mic * 1000 
 
-    return future_metric_value
+    return future_Unit_Metric_mic_value
     
-
-
-
-#mile na jednostki imprerialne
-def imperial_conversion(future_imperial, future_imperial_value):
+def UnitImperialConversion(future_imperial, future_imperial_value):
      
     match future_imperial:
         case "in":
@@ -73,77 +78,86 @@ def imperial_conversion(future_imperial, future_imperial_value):
 
     return future_imperial_value
 
+def InImperial_OutMetric(in_imperial):
+    out_Unit_Metric_mic = in_imperial * 0.000621371
+    return out_Unit_Metric_mic
 
-
-#mile na metry
-def imperial_to_metric(in_imperial):
-    out_metric = in_imperial * 0.000621371
-    return out_metric
-
-#metry na mile
-def metric_to_imperial(in_metric):
-    out_imperial = in_metric / 1609.344
+def InMetric_OutImperial(in_Unit_Metric_mic):
+    out_imperial = in_Unit_Metric_mic / 1609.344
     return out_imperial
 
+###########################################################
+#####                            input_area
 
-x = input("Podaj długość wraz z jednostką w formacie: mm, cm, m, km: ")
-        #if mm,m,dm,cm to wykonać dla metrycznych itp
-x = x.split(" ")
+def data_gathering():
+    com = "Podaj długość wraz z jednostką w formacie \n metrycznym:\n mm, cm, m, km:\n lub imperialnym:\n in, ft, yd, fur, mi \n >>"
+    x = input(com)
+    x = x.split(" ")
+    return x
     
-
-# dodać funkcję, która pozwoli na przypisanie zawsze mm cm dm m km dla jednostki w tym miejscu listy, 
-# bez względu na formę wkładu przez klienta
-wartosc_in = x[0]
-wartosc_in = float(wartosc_in)
-jednostka_in = x[1]
-jednostka_out = input("Na co zamienić tą długość? ")
-
-#określenie systemu in, przeliczenie na bazową jednostkę
-if jednostka_in == "mm" or "cm" or "m" or "dm" or "km":
-    system_in = "metric"
-    wartosc_metr = metric(na_metr = wartosc_in, metr = jednostka_in)
+def inputdata(x):
+    Value_in = x[0]
+    Value_in = float(Value_in)
+    Unit_in = x[1]
+    Unit_out = input("Na co zamienić tą długość?\n Format metryczny??:\n mm, cm, m, km:\n Format imperialny??:\n in, ft, yd, fur, mi \n >>")
+    return Value_in, Unit_in, Unit_out
     
+def datacleaning():
+    #CREATE DATA CLEANING
+    pass
+
+def try_except():
+    #CREATE TRY EXCEPTS
+    pass
+
+clientANS = data_gathering()
+valueIN, unitIN, unitOUT = inputdata(x = clientANS)
+datacleaning()
+try_except()
+
+#OUT: valueIN, unitIN, unitOUT
+
+
+###########################################################
+#####                            execution_area
+
+#determining the type of conversion, and creating stratefy for converter
+
+def system_determining(Value_in, Unit_in, Unit_out):
     
-elif jednostka_in == "in" or "ft" or "yd" or "mi":
-    system_in = "imperial"
-    wartosc_mila = imperial(na_mile = wartosc_in, mila = jednostka_in)
+    if Unit_in == "mm" or "cm" or "m" or "dm" or "km":
+        system_in = "metric"
+        valueMID = BaseValueMetric(ValueMetric_m = Value_in, UnitMetric_m = Unit_in)
+    elif Unit_in == "in" or "ft" or "yd" or "mi":
+        system_in = "imperial"
+        valueMID = BaseValueImperial(ValueImperial_mi = Value_in, UnitImperial_mi = Unit_in)
+        
+    if Unit_out == "mm" or "cm" or "m" or "km":
+        system_out = "metric"
+    elif Unit_out == "in" or "ft" or "yd" or "mi":
+        system_out = "imperial"
 
+    return valueMID, system_in, system_out
 
-#określenie systemu out
-if jednostka_out == "mm" or "cm" or "m" or "km":
-    system_out = "metric"
+def converter_strategy(system_in, system_out):
+    if system_in == system_out == "metric":
+        valueOUT = UnitMetricConversion(future_Unit_Metric_mic = value_m, future_Unit_Metric_mic_value= unitOUT)
+
+    elif system_in == system_out == "imperial":
+        valueOUT = UnitImperialConversion(future_imperial_value = valueOUT, future_imperial = unitOUT)
     
-elif jednostka_out == "in" or "ft" or "yd" or "mi":
-    system_out = "imperial"
+    elif system_in == "metric" and system_out == "imperial":
+        pass
 
+    elif system_in == "imperial" and system_out == "metric":
+        pass
 
-#porównanie systemów, wybór straregii konwersji
-if system_in == system_out == "metric":
-    wynik = metr_conversion(future_metric = jednostka_in, future_metric_value = wartosc_metr)
+    return valueOUT
 
+valueMID, systemIN, systemOUT = system_determining(Value_in = valueIN, Unit_in = unitIN, Unit_out = unitOUT)
+valueFINAL = converter_strategy(system_in = systemIN, system_out = systemOUT)
 
+###########################################################
+#####                            output_area
 
-
-elif system_in == system_out == "imperial":
-    wynik = imperial_conversion(future_imperial = jednostka_in, future_imperial_value = wartosc_mila)
-
-
-
-
-elif system_in == "metric" and system_out == "imperial":
-    wartosc_imperial = metric_to_imperial(in_metric = wartosc_metr)
-    wynik = imperial(mila = "mi", na_mile = wartosc_imperial)
-
-
-
-
-
-elif system_in == "imperial" and system_out == "metric":
-    out_metric = imperial_to_metric(in_imperial = wartosc_mila)
-    wynik = metr_conversion(future_metric = "m", future_metric_value = out_metric)
-
-
-
-
-    
-print(wynik, jednostka_out)
+print(valueFINAL, unitOUT)
