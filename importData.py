c2ff24dcaf9726da67fb3025767ab43754bf1a5b
dawid <86666763+ddkcz@users.noreply.github.com>
2023-12-21 00:42:35 +0100

    Adding first data cleaning on CRM system

---
+++ importData.py
@@ -0,0 +1,22 @@
+
+def import_data()
+    import pandas as pd
+
+
+    df = pd.read_csv("clients.csv")
+
+
+    df = df.apply(lambda x: x.str.strip() if x.dtype == "O" else x)
+    df['NAZWA'] = df['NAZWA'].str.upper()
+    df['E-MAIL'] = df['E-MAIL'].str.lower()
+    df['SUPERTYP'] = df['SUPERTYP'].str.lower()
+    df['TYP'] = df['TYP'].str.lower()
+    df['SUBTYP'] = df['SUBTYP'].str.lower()
+
+
+    df = df.fillna('')
+
+    df.to_csv('clients_clean.csv', index=False)
+
+    print(df)
+
